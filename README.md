44e85d1f91fa0072ae67cbb444e286a7093f6102
crowe73 <rowechad73@yahoo.com>
2022-03-07 23:19:07 -0600

    Add draw effect
    Change grid cell's background color to black when left mouse button
    is held down and mouse pointer is moved.
    
    Add margin and padding of 0 to all elements in style.css file.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 03/05/2022 21:21 - Added index.html, style.css, and javascript.js files.
 
 03/07/2022 00:06 - Created 16 x 16 grid. Refactored style.css file.
+
+03/07/2022 23:12 - Added draw effect.
--- javascript.js
+++ javascript.js
@@ -1,8 +1,39 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
 
 // Add 16 divs.
 for (let i = 0; i < 256; i++)
 {
-    const div = document.createElement('div');
-    container.appendChild(div);
+    const cell = document.createElement('div');
+    cell.addEventListener('mouseover', changeColor);
+    cell.addEventListener('mousedown', changeColor);
+    container.appendChild(cell).className = 'grid-items';
+}
+
+var mouseDown = false;
+
+document.body.onmousedown = function()
+{
+    mouseDown = true;
+};
+
+document.body.onmouseup = function()
+{
+    mouseDown = false;
+};
+
+// Change cell color.
+function changeColor(e)
+{
+    if (mouseDown)
+    {
+        //let color1 = Math.floor(Math.random() * 256);
+        //let color2 = Math.floor(Math.random() * 256);
+        //let color3 = Math.floor(Math.random() * 256);
+
+        let color1 = 0;
+        let color2 = 0;
+        let color3 = 0;
+
+        e.target.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+    }
 }
